7a13cbe88e95c660dc9dff386ae8d0585ad5b834
Alawnely <lawrenceg.0308@outlook.ph>
2025-09-09 08:35:06 +0800

    feat: real error page

--- src/app/reviewers-hub/page.tsx
+++ src/app/reviewers-hub/page.tsx
@@ -1,6 +1,6 @@
 import styles from './reviewers-hub.module.css';
 
-export default function About() {
+export default function ReviewersHub() {
   return (
     <div className={`${styles.notFound} pb-20 flex items-center`}>
       <h1 className="text-5xl font-bold">Hold tight! We're making some improvements.</h1>
